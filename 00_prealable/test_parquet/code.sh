829a9f8a7ffe9d290a9c2959e09097bc8536c4e7
Jean-Bernard DIEN <JBDien@users.noreply.github.com>
2023-12-03 21:43:38 +0100

    Create code.sh

---
+++ 00_prealable/test_parquet/code.sh
@@ -0,0 +1,39 @@
+
+
+apt install python3-pip
+pip install numpy pandas pyarrow
+
+## Exemple python
+
+# import des libs
+#!/usr/bin/python3
+import pyarrow as pa
+import pandas as pd
+import numpy as np
+import pyarrow.parquet as pq
+
+# création du dataframe
+df = pd.DataFrame({'col1': [1.0,2.1,3.2], 'col2': ['paul','pierre','jean'],'col3': ['paris','marseille','rouen']})
+
+# conversion en pyarrow
+table = pa.Table.from_pandas(df)
+
+# écriture du fichier en parquet
+pq.write_table(table, 'example.parquet')
+
+# check chargement fichier et lecture
+table2 = pq.read_table('example.parquet')
+table2.to_pandas()
+
+
+# Site officiel Apache Parquet 
+https://parquet.apache.org/
+
+wget https://github.com/Teradata/kylo/raw/master/samples/sample-data/parquet/userdata1.parquet
+
+pip install parquet-cli
+
+parq userdata1.parquet
+parq userdata1.parquet --head 3
+parq userdata1.parquet --tail 3
+parq userdata1.parquet --count
